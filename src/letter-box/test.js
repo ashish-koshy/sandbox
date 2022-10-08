5035b69061170beb3a31782ac35eb818b28b2a86
Ashish Koshy <ashish.avior@gmail.com>
2022-10-09 02:05:10 +0530

    completed letter-box solution

--- src/letter-box/test.js
+++ src/letter-box/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var exercise = require('./main.js');
 describe('Paint letter boxes', function () {
-    it('Test 1', function () {
+    it('Should produce the output - 1963011111', function () {
         assert.equal(
             exercise.letterBox(125, 132).join(''),
             [1, 9, 6, 3, 0, 1, 1, 1, 1, 1].join('')
