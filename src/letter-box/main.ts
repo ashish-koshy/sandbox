6e7980428cfc442b9b22c43bf3b663a6560905a8
Ashish Koshy <ashish.avior@gmail.com>
2022-10-09 02:04:21 +0530

    completed letter-box solution

--- src/letter-box/main.ts
+++ src/letter-box/main.ts
@@ -1,6 +1,17 @@
+const map = new Map<string, number>();
+
+const splitNumber = (num: number) => {
+    if (!num) return;
+    const key = `${num % 10}`;
+    const counter = 1;
+    if (map.has(key)) map.set(key, (map.get(key) || 0) + 1);
+    else map.set(key, counter);
+    splitNumber(Math.trunc(num / 10));
+};
+
 export const letterBox = (start = 0, end = 0) => {
-    for (let i = start; i <= end; i++)
-        // eslint-disable-next-line no-console
-        console.info(i);
-    return 'Hello world!';
+    for (let i = start; i <= end; i++) splitNumber(i);
+    const result = new Array<number>(map.size);
+    for (let i = 0; i <= map.size; i++) result[i] = map.get(`${i}`) || 0;
+    return result;
 };
--- src/letter-box/test.js
+++ src/letter-box/test.js
@@ -2,6 +2,9 @@ var assert = require('assert');
 var exercise = require('./main.js');
 describe('Paint letter boxes', function () {
     it('Test 1', function () {
-        assert.equal(exercise.letterBox(), 'Hello world!');
+        assert.equal(
+            exercise.letterBox(125, 132).join(''),
+            [1, 9, 6, 3, 0, 1, 1, 1, 1, 1].join('')
+        );
     });
 });
