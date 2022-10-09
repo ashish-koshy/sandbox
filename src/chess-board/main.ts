283ffc231923aece5e69c5927ce374f24efb78a1
Ashish Koshy <ashish.avior@gmail.com>
2022-10-09 05:43:07 +0530

    completed chess-board

--- src/chess-board/main.ts
+++ src/chess-board/main.ts
@@ -1,1 +1,15 @@
-export const chessBoard = () => `Hello world!`;
+const white = 'O',
+    black = 'X';
+
+const invertColumn = (columns: string[]) =>
+    columns.map((item: string) => (item === white ? black : white)) as string[];
+
+export const chessBoard = (rowCount = 0, columnCount = 0) => {
+    const board: string[][] = [];
+    const columns: string[] = [];
+    for (let column = 0; column < columnCount; column++)
+        columns.push(column % 2 === 0 ? white : black);
+    for (let row = 0; row < rowCount; row++)
+        board.push(row % 2 === 0 ? columns : invertColumn(columns));
+    return board;
+};
--- src/chess-board/test.js
+++ src/chess-board/test.js
@@ -1,7 +1,28 @@
 var assert = require('assert');
 var exercise = require('./main.js');
 describe('Display Chess Board', function () {
-    it('Test 1', function () {
-        assert.equal(exercise.chessBoard(), 'Hello world!');
+    it('Should generate a 6x4 chess board', function () {
+        assert.equal(
+            JSON.stringify(exercise.chessBoard(6, 4)),
+            JSON.stringify([
+                ['O', 'X', 'O', 'X'],
+                ['X', 'O', 'X', 'O'],
+                ['O', 'X', 'O', 'X'],
+                ['X', 'O', 'X', 'O'],
+                ['O', 'X', 'O', 'X'],
+                ['X', 'O', 'X', 'O'],
+            ])
+        );
+    });
+
+    it('Should generate a 3x7 chess board', function () {
+        assert.equal(
+            JSON.stringify(exercise.chessBoard(3, 7)),
+            JSON.stringify([
+                ['O', 'X', 'O', 'X', 'O', 'X', 'O'],
+                ['X', 'O', 'X', 'O', 'X', 'O', 'X'],
+                ['O', 'X', 'O', 'X', 'O', 'X', 'O'],
+            ])
+        );
     });
 });
